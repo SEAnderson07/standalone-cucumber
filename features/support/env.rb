f3d59ac92f6befcb8055ae7dce26a708fd63621c
Sarah Anderson <seanderson07@gmail.com>
2014-12-04 22:12:26 -0500

    Added more specific capybara configuration settings. Required a specific version of the selenium-webdriver gem.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,18 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'selenium-webdriver'
 
 Capybara.configure do |config|
+  config.run_server = false
   config.default_driver = :selenium
   config.app_host   = 'http://www.google.com'
 end
+
+Capybara.register_driver :selenium do |app|
+  Selenium::WebDriver::Firefox::Binary.path = "/opt/firefox/firefox-bin"
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
+
 World(Capybara)
+
